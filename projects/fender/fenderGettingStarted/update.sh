4c966294cc99e1272189c7036e750eba432f9f39
Arroganz <felix.ganz@epitech.eu>
2018-01-10 13:00:16 +0100

    updated update.sh

--- projects/fender/fenderGettingStarted/update.sh
+++ projects/fender/fenderGettingStarted/update.sh
@@ -2,14 +2,30 @@
 
 # Cloning into teamfcm/futils
 
-rm -rf dl_futils
-git clone -b master --single-branch http://github.com/ganz_f/futils dl_futils --depth=1
 
-# Cloning ok
-echo "Ok - Recovered latest Futils version."
 
-rm -rf ./futils
-cp -r dl_futils/utils ./futils
-cp dl_futils/projects/fender/release/libfender.a ./lib
-cp dl_futils/projects/fender/include/*.hpp ./include
-rm -rf dl_futils
+if [ ! -d ".dl_futils" ]; then
+    echo " - Downloading futils..."
+    git clone -b master --single-branch git@github.com:ganzf/futils.git .dl_futils --depth=1
+    echo " - Done."
+fi
+
+cd .dl_futils
+echo "Fetching update..."
+git pull origin master
+cd ..
+
+if [ ! -d "./futils" ]; then
+   mkdir ./futils
+fi
+
+ls ./futils > oldUtils
+cp .dl_futils/utils/* ./futils/
+ls ./futils > newUtils
+diff oldUtils newUtils
+rm -f oldUtils
+rm -f newUtils
+cp .dl_futils/projects/fender/release/libfender.a ./lib
+cp .dl_futils/projects/fender/include/*.hpp ./include
+
+echo "Update complete."
