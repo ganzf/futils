7e8fe4a5533f00bcdfbc6514e486093a04b9f42f
Arroganz <felix.ganz@epitech.eu>
2018-01-10 21:05:40 +0100

    fender.hpp ok

--- docs/fender/Fender.rst
+++ docs/fender/Fender.rst
@@ -1,2 +1,58 @@
 Fender
 ======
+
+.. rst-class:: fa fa-file-text-o
+
+    \- Defined in ``fender.hpp``
+
+It all starts with ``fender::Fender``.
+
+Here's what it looks like :
+
+.. code-block:: cpp
+
+   class Fender
+   {
+   public:
+        Fender(std::string const &av_0);
+
+        int start();
+        int run();
+
+        template <typename System, typename ...Args>
+        void addSystem(Args ...args);
+
+		    template <typename System>
+		    void addSystem();
+		    
+        template <typename System>
+        void removeSystem();
+    };
+
+Fender(string const &)
+======================
+
+Fender requires ``std::string const &`` to ``av[0]`` in order to make all relative paths work properly.
+
+int start()
+===========
+
+Starting the engine is the second function call you should make. It will let the engine initialize itself.
+Right after starting the engine, you can add your own systems.
+
+addSystem<T>(...)
+=================
+
+Use this function to add your own systems after starting the engine.
+
+removeSystem<T>
+===============
+
+Use this function to remove a system (it can be one added by default, or your own).
+
+Once you're set, simply run the engine.
+
+int run()
+=========
+
+This function returns the engine status. You should call this function only once when you're all set.
--- projects/fender/include/fender.hpp
+++ projects/fender/include/fender.hpp
@@ -34,21 +34,21 @@ namespace fender
     using vec2ui = futils::Vec2<unsigned int>;
     using vec3ui = futils::Vec3<unsigned int>;
 
-    class Fender
-    {
-        futils::UP<futils::EntityManager> entityManager;
-        futils::UP<futils::Mediator> events;
-    public:
-        Fender(std::string const &);
-        int start();
-        int run();
+  class Fender
+  {
+    futils::UP<futils::EntityManager> entityManager;
+    futils::UP<futils::Mediator> events;
+  public:
+    Fender(std::string const &);
+    int start();
+    int run();
 
-        void loadSystemDir(std::string const &path);
+    void loadSystemDir(std::string const &path);
 
-        template <typename System, typename ...Args>
-        void addSystem(Args ...args) {
-            entityManager->addSystem<System>(args...);
-        };
+    template <typename System, typename ...Args>
+    void addSystem(Args ...args) {
+      entityManager->addSystem<System>(args...);
+    };
 
 		template <typename System>
 		void addSystem()
@@ -56,16 +56,16 @@ namespace fender
 			entityManager->addSystem<System>();
 		}
 		
-        template <typename System>
-        void removeSystem() {
-            entityManager->removeSystem(futils::demangle<System>());
-        }
+    template <typename System>
+    void removeSystem() {
+      entityManager->removeSystem(futils::demangle<System>());
+    }
 
-        template <typename T, typename ...Args>
-        T *createEntity(Args ...args) {
-            return entityManager->create<T>(args...);
-        };
+    template <typename T, typename ...Args>
+    T *createEntity(Args ...args) {
+      return entityManager->create<T>(args...);
     };
+  };
 }
 
 #endif
