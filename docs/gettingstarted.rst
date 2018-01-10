10eee9c99de23d8c5105188e12fdcd3344c2e706
Arroganz <felix.ganz@epitech.eu>
2018-01-10 18:31:44 +0100

    updated update.sh

--- docs/gettingstarted.rst
+++ docs/gettingstarted.rst
@@ -28,3 +28,52 @@ Use ``all.sh`` for all of the above.
 .. rst-class:: fa fa-warning
 
    > The update.sh script will clone futils in a hidden folder.
+
+How to use
+----------
+
+In the newly created directory, you'll find basic main.cpp
+
+.. code-block:: cpp
+
+   # include "fender.hpp"
+
+   int main(int, char **av)
+   {
+                fender::Fender engine(av[0]);
+
+                if (engine.start() != 0)
+                  return 1;
+                engine.run();
+                return 0;
+   }
+
+This is a minimal working usage of the engine. Default systems will be loaded (by default it loads the SFMLSystems).
+If you want to add your own systems, here's where you should do it.
+
+.. code-block:: cpp
+
+   # include "fender.hpp"
+   # include "mySystem.hpp"
+
+   // mySystem.hpp
+   class mySystem : public futils::ISystem
+   {
+                (...)
+   public:
+                mySystem(bool someValue);
+                void run(float) override;
+   };
+
+   int main(int, char **av)
+   {
+                fender::Fender engine(av[0]);
+
+                if (engine.start() != 0)
+                  return 1;
+                engine.addSystem<mySystem>(true);
+                engine.run();
+                return 0;
+                
+   }
+
--- projects/fender/fenderGettingStarted/update.sh
+++ projects/fender/fenderGettingStarted/update.sh
@@ -28,4 +28,44 @@ rm -f newUtils
 cp .dl_futils/projects/fender/release/libfender.a ./lib
 cp .dl_futils/projects/fender/include/*.hpp ./include
 
+if [ ! -d "./futils/Components" ]; then
+    echo "Creating ./futils/Components..."
+    mkdir -p ./futils/Components/
+    echo " - Done";
+fi
+
+if [ ! -d "./futils/Entities" ]; then
+    echo "Creating ./futils/Entities..."
+    mkdir -p ./futils/Entities/
+    echo " - Done";
+fi
+
+if [ ! -d "./futils/Systems" ]; then
+    echo "Creating ./futils/Systems..."
+    mkdir -p ./futils/Systems/
+    echo " - Done";
+fi
+
+echo " - Fetching fender *.hpp";
+ls ./futils/Components > oldComponents
+find .dl_futils/projects/fender/src/Components -type f -name '*.hpp' -exec cp {} ./futils/Components \;
+ls ./futils/Components > newComponents
+echo "New Components : (may be empty)"
+diff oldComponents newComponents
+rm -f oldComponents newComponents
+
+ls ./futils/Entities > oldEntities
+find .dl_futils/projects/fender/src/Entities -type f -name '*.hpp' -exec cp {} ./futils/Entities \;
+ls ./futils/Entities > newEntities
+echo "New Entities : (may be empty)"
+diff oldEntities newEntities
+rm -f oldEntities newEntities
+
+ls ./futils/Systems > oldSystems
+find .dl_futils/projects/fender/src/Systems -type f -name '*.hpp' -exec cp {} ./futils/Systems \;
+ls ./futils/Systems > newSystems
+echo "New Systems : (may be empty)"
+diff oldSystems newSystems
+rm -f oldSystems newSystems
+
 echo "Update complete."
